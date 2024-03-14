147f56f722456e0f0caaaaa87ead1abd86a1bcb8
Pradeesh kumar <131758539+pradxxsh@users.noreply.github.com>
2024-03-14 08:44:19 +0530

    Create MainActivity2.java

---
+++ app/src/main/java/com/example/explicitintent/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.explicitintent.MainActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void homeScreen(View view) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+}
